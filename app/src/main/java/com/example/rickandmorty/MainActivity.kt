b60e1ea2cc66afc4073a7893315319bdde8b5f44
Ilya Ponomaryov <i.ponomaryov.00@gmail.com>
2023-01-05 23:28:04 +0500

    Remove setupNavigation

--- app/src/main/java/com/example/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/example/rickandmorty/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupNavigation()
+
     }
 
     private fun setupNavigation() = with(binding) {
